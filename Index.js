050ef1badf6901a8b8bd10f7b77ed41fd18d5ec5
anajangulashvilidoit04-ui <ana.jangulashvilidoit04@geolab.edu.ge>
2026-08-10 01:00:14 +0400

    Add product fetching and display functionality

---
+++ Index.js
@@ -0,0 +1,113 @@
+let products = [];
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+
+    products = data.products;
+
+    displayProducts(products);
+    displayCartSummary();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchProducts();
+
+const searchInput = document.querySelector(".search-value");
+
+searchInput.addEventListener("input", (event) => {
+  const value = event.target.value.toLowerCase();
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(value),
+  );
+
+  displayProducts(filteredProducts);
+});
+
+const displayProducts = (products) => {
+  const productsList = document.querySelector(".products-list");
+
+  productsList.innerHTML = "";
+
+  products.forEach((product) => {
+    const {
+      title,
+      price,
+      description,
+      brand,
+      stock,
+      images,
+      quantity,
+    } = product;
+
+    const productCard = document.createElement("div");
+
+    productCard.classList.add("card");
+
+    const productImage = images[0];
+
+    productCard.innerHTML = `
+      <img src="${productImage}" class="thumbnail" />
+
+      <div class="product-info">
+        <h3>${title}</h3>
+
+        <h4>${Math.round(price).toLocaleString()} GEL</h4>
+
+        <p>${description}</p>
+
+        <p>Brand: ${brand}</p>
+
+        <p>Stock: ${stock}</p>
+
+        <button class="decrease">-</button>
+
+        <span class="quantity">
+          Quantity: ${quantity || 0}
+        </span>
+
+        <button class="increase">+</button>
+      </div>
+    `;
+
+    const decreaseBtn = productCard.querySelector(".decrease");
+    const increaseBtn = productCard.querySelector(".increase");
+    const currentQuantity = productCard.querySelector(".quantity");
+
+    decreaseBtn.addEventListener("click", () => {
+      if (product.quantity > 0) {
+        product.quantity--;
+
+        currentQuantity.textContent = `Quantity: ${product.quantity}`;
+
+        displayCartSummary();
+      }
+    });
+
+    increaseBtn.addEventListener("click", () => {
+      product.quantity++;
+
+      currentQuantity.textContent = `Quantity: ${product.quantity}`;
+
+      displayCartSummary();
+    });
+
+    productsList.appendChild(productCard);
+  });
+};
+
+const displayCartSummary = () => {
+  const totalPrice = document.querySelector(".total-price");
+
+  const priceSum = products.reduce((acc, currentProduct) => {
+    return acc + currentProduct.price * (currentProduct.quantity || 0);
+  }, 0);
+
+  totalPrice.textContent = `Total Price: ${Math.round(
+    priceSum,
+  ).toLocaleString()} GEL`;
+};
